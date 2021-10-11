91d1682a9e87bbce3010dd19f2c737e5a69f0f0d
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2021-10-11 18:35:40 +0200

    (install node modules before testing)

---
+++ Web Scraper/index.js
@@ -0,0 +1,29 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+const url= "https://www.theguardian.com/international";
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        // console.log(html);
+        const $ = cheerio.load(html);
+        const articles = [];
+
+
+        $(".js-headline-text", html).each(function(){
+          const title = $(this).text();
+          const url = $(this).attr("href"); //instead of : const url = $(this).find("a").attr("href");
+          articles.push({
+              title,
+              url
+          });
+        });
+        console.log(articles);
+    }).catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
