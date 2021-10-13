585badad7a65182713468ab87336e892b3870167
João Almeida <jvastos@gmail.com>
2021-10-14 00:11:28 +0200

    input field linked with Date and Storage + autofill

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-//GET VALUES FROM USER INPUT (NOT FINAL)
-
-let birthdayDay = document.getElementById("birthdayDay");
-let birthdayMonth = document.getElementById("birthdayMonth");
-let birthdayYear = document.getElementById("birthdayYear");
-
-//ASSOCIATE VALUES IN THE SAME OBJECT
-let mybirthday = {
-  day: birthdayDay.value,
-  month: birthdayMonth.value,
-  year: birthdayYear.value
+//AUTO FILL THE INPUT FIELD, IF LOCALSTORAGE ALREADY HAD A VALUE (PROBABLY FROM A PREVIOUS INTERACTION WITH THE SITE)
+if(localStorage.length !== 0) {
+  document.getElementById("date").value = localStorage.getItem("birthdayString");
 }
 
-// SAVE BIRTHDAY OBJECT TO LOCAL STORAGE
+//FUNCTION FOR WHAT HAPPENS WHEN THE SUBMIT BUTTON IS PRESSED
+document.getElementById("submit").addEventListener("click", function submitBreak(e){
+
+  //PREVENT THE BUTTON FROM ACTUALLY SEND THE FORM (THROUGH A POST METHOD, FOR EXAMPLE)
+  e.preventDefault();
+
+  let inputDate = document.getElementById("date").value;
+
+  //GET VALUES FROM USER INPUT
+  let birthdayDay = inputDate.substring(8);
+  let birthdayMonth = inputDate.substring(5, 7);
+  let birthdayYear = inputDate.substring(0, 4);
+  
+  console.log(inputDate);
+  console.log(birthdayDay);
+  console.log(birthdayMonth);
+  console.log(birthdayYear);
+
+  //ASSOCIATE VALUES IN THE SAME OBJECT
+  let mybirthday = {
+    day: birthdayDay,
+    month: birthdayMonth,
+    year: birthdayYear
+  }
+
+  // SAVE BIRTHDAY OBJECT TO LOCAL STORAGE
 localStorage.setItem("birthday", JSON.stringify(mybirthday));
+localStorage.setItem("birthdayString", inputDate);
 
 // RETRIEVE BIRTHDAY OBJECT FROM LOCAL STORAGE
 let savedBirthday = JSON.parse(localStorage.getItem("birthday"));
@@ -38,3 +56,15 @@ let ageDays = ageYears * 365 + (currentDate.month * 30) + currentDate.day;
 document.getElementById("slot1").innerHTML = "You have lived " + ageYears + " years " + ageMonths + " months " + ageDays + " days without meeting this guy. But you are part of the same Zodiac family. How cool?" 
 
 console.log("You have lived " + ageYears + " years " + ageMonths + " months " + ageDays + " days without meeting this guy. But you are part of the same Zodiac family. How cool?");
+
+});
+
+
+
+
+
+
+
+
+
+
