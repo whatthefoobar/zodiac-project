ee3cb876de96431197f2887aa634fc74b0f76e50
RichiKennedy <richard.kennedy@hyperisland.se>
2021-10-18 17:35:14 +0200

    respnsive updates

--- app.js
+++ app.js
@@ -12,9 +12,26 @@ console.log(document.querySelector(".birthday-form").classList);
 }
 
 function submitSlide() {
-  document.querySelector(".submit-slide").classList.toggle("submit-active"); 
+  document.querySelector(".submit-slide").classList.toggle("submitActive"); 
+}
+function slideBack() {
+  document.querySelector(".submit-slide").classList.add("submitInactive"); 
+  
 }
 
+// const form =document.querySelector(".submit-slide"); //the form
+// const exit =document.querySelector(".far");// i dunno how you'd name them
+// const formBtn = document.querySelector("#submit");// the form btn
+// formBtn.addEventListener(click, submitActive);
+// exit.addEventListener(click, submitInactive);
+// function slideOut(){
+//     form.classList.add("submitActive");// where .slideout is your css class that does the slideout to result
+// }
+// //similar
+// function slideIn(){
+//     form.classList.remove("submit-inactive");// where .slideout is your css class that does the slideout to result
+//     form.classList.add("");// where .slidein is your css class that gets back to the form page
+// }
 
 
 
